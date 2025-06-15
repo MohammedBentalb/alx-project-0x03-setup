9f77e37188e6f11bf80d7bfb211d875fff1cfb1c
mohammed bentalb <mohammedbentalb2000@gmail.com>
2025-06-15 12:47:09 +0100

    modifying files

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { LayoutProps } from "@/interface";
 
-interface LayoutProps {
+const Layout: React.FC<LayoutProps> = ({
+  children,
+}: {
   children: ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+}) => {
   return (
     <>
       <Header />
